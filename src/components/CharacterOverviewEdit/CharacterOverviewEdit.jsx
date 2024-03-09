f43906776957a47e25ffdea0728caa9a7bf6667b
Beth Tracy <bntracy@gmail.com>
2024-03-09 15:35:07 -0600

    first edit form displays. Does not save yet

--- src/components/CharacterOverviewEdit/CharacterOverviewEdit.jsx
+++ src/components/CharacterOverviewEdit/CharacterOverviewEdit.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
 function CharacterOverviewEdit({setIsEditing}) {
+    const character = useSelector(store => store.character);
+    const [newSpecies, setNewSpecies] = useState(character.species);
+    const [newGender, setNewGender] = useState(character.gender);
+    const [newPhysicalDescription, setNewPhysicalDescription] = useState(character.physical_description);
+
     return <>
-        <p>Edit form goes here</p>
-        <button>Save</button>
-        <button onClick={()=>setIsEditing(false)}>Cancel</button>
+        <form>
+            <div>
+                <label htmlFor="species">Species: </label>
+                <input type="text" name="species" value={newSpecies} onChange={event => setNewSpecies(event.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="gender">Gender: </label>
+                <input type="text" name="gender" value={newGender} onChange={event => setNewGender(event.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="physical-description">Physical Description: </label>
+                <input type="text" name="physical-description" value={newPhysicalDescription} onChange={event => setNewPhysicalDescription(event.target.value)}/>
+            </div>
+            <button>Save</button>
+            <button onClick={()=>setIsEditing(false)}>Cancel</button>
+        </form>
     </>;
 }
 
